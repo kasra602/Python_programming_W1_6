de6e2be4ce33a80f885ce4787f663b897f285768
Kasra zahedi <106477508+kasra602@users.noreply.github.com>
2026-09-19 22:41:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 Value = int(input("Insert an integer:"))
 Remainder = Value % 2
 print ("Value is", Value)
-print ("The remainder is", Remainder, "when", Value, "is divided by 2")
+print ("The remainder is", Remainder, "when", Value, "is divided by 2.")
